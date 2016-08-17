dcd6b19f11f696b675551cc370e3b2048a2fe640
nikhit adusumilli <nikhit adusumilli>
2016-08-17 02:31:11 -0500

    changes to the files

--- config/app.php
+++ config/app.php
@@ -149,7 +149,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        //Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -209,8 +210,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Illuminate\Html\FormFacade::class,
-        'Html' => Illuminate\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        //'Form' => Illuminate\Html\FormFacade::class,
+        //'Html' => Illuminate\Html\HtmlFacade::class,
     ],
 
 ];
